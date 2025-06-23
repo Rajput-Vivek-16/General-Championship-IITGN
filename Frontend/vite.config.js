ed625d4b43a2765e75cb15824fe1cc4f1392d99b
Rajput Vivek <167712861+Rajput-Vivek-16@users.noreply.github.com>
2025-06-23 22:55:25 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // Use relative paths
+  base: '/General-Championship-IITGN/',  // Use relative paths
   plugins: [react()],
 });
